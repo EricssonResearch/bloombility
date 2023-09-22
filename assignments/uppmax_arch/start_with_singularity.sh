d99fda8095c816318705a4ce02fa96c67f8e729d
Hwasser <victorhwasser@gmail.com>
2023-09-22 14:01:44 +0200

    Singularity script now working

--- assignments/uppmax_arch/start_with_singularity.sh
+++ assignments/uppmax_arch/start_with_singularity.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+# Brief: A Script which boots up a federated learning server and N clients
+# Author: Victor Hwasser
+# TODO: The proper way to start is by using "singularity instance start .."
+#       but this do not work for some reason, it starts an empty container,
+#       but "singularity run .." works perfectly.
+
 echo "How many devices to use for training?"
 read n_devices
 
 if [ $n_devices -gt 0 ]
 then
 	# Start server from "start_server" script
-	singularity instance start test_server/server.sif fl_server
+	echo "Starting server"
+	# singularity instance start test_server/server.sif fl_server --command "python3 /app/server.py"
+	singularity run test_server/server.sif &
 	# Start all devices
 	i=1
 	while [ $i -le $n_devices ]
 	do
 		echo "Starting client$i"
-		singularity instance start test_client/client.sif fl_client$i
+		# singularity instance start test_client/client.sif fl_client$i --command "./test_client/entrypoint.sh" 
+		singularity run test_client/client.sif &
 		let "i+=1"
 	done
 else
