fd42c3012b95b3d5ff5b2e814c1d0c04718dd4b1
George Liakopoulos <liakopoulosgeo10@gmail.com>
2023-10-06 10:15:59 +0200

    Updated files with pre-commit

--- bloom/load_data/download_cifar10.py
+++ bloom/load_data/download_cifar10.py
@@ -1,16 +1,14 @@
-
-from torchvision.datasets import CIFAR10 , utils
+from torchvision.datasets import CIFAR10, utils
 import torchvision.transforms as transforms
 import torch
-import os 
+import os
 
 
 class CIFARTEN(CIFAR10):
     """
-        A class used to represent and handle the CIFAR10 dataset
+    A class used to represent and handle the CIFAR10 dataset
 
     """
-
 
     def __init__(
         self, root, train=True, transform=None, target_transform=None, download=False
@@ -45,8 +43,7 @@ class CIFARTEN(CIFAR10):
                 "Dataset not found." + " You can use download=True to download it"
             )
 
-        return 
-
+        return
 
     def check_files_exist(self):
         """Checks whether expected files exist
@@ -54,8 +51,15 @@ class CIFARTEN(CIFAR10):
         Returns:
             a boolean value that indicates whether all files were found
         """
-        data_files = [ "data_batch_1" , "data_batch_2" , "data_batch_3" , "data_batch_4" , "data_batch_5" , "test_batch"]
-        self.data_folder = os.path.join(self.root , "cifar-10-batches-py")
+        data_files = [
+            "data_batch_1",
+            "data_batch_2",
+            "data_batch_3",
+            "data_batch_4",
+            "data_batch_5",
+            "test_batch",
+        ]
+        self.data_folder = os.path.join(self.root, "cifar-10-batches-py")
         exists = True
         for file in data_files:
             cur = os.path.join(self.data_folder, file)
@@ -71,29 +75,25 @@ class CIFARTEN(CIFAR10):
         Returns:
             a boolean value that indicates whether all folders were found
         """
-        self.data_folder = os.path.join(self.root , "cifar-10-batches-py")
+        self.data_folder = os.path.join(self.root, "cifar-10-batches-py")
         if os.path.isdir(self.data_folder):
             return True
         else:
             return False
 
-    
-    def get_cifar10_datasets(root , transform):
+    def get_cifar10_datasets(root, transform):
         """Downloads the train and test sets of CIFAR10
 
         Returns:
             the train-set and the test-set
         """
 
-        trainset = CIFARTEN(
-            root=root, train=True, download=True, transform=transform
-        )
+        trainset = CIFARTEN(root=root, train=True, download=True, transform=transform)
 
-        testset = CIFARTEN(
-            root=root, train=False, download=True, transform=transform
-        )
+        testset = CIFARTEN(root=root, train=False, download=True, transform=transform)
 
         return trainset, testset
+
 
 """
 Example of using this class:
@@ -101,4 +101,4 @@ Example of using this class:
 if __name__ == "__main__":
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     trainset, testset = CIFARTEN.get_cifar10_datasets('./data',transform)
-""" 
+"""
