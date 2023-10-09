af081df89a14ad57f952f45f1ffe529106e2f10f
Darankru <d.krummrich@gmx.net>
2023-10-09 13:13:18 +0200

    adapted asserts to new config structure

--- bloom/tests/test_configs.py
+++ bloom/tests/test_configs.py
@@ -1,88 +1,76 @@
-import os
-import sys
-import yaml
-
-default_config = "default_config.yaml"
-
-# ============================== read yaml ==============================================
+from context import config
 
 
-def read_config_file(config_filepath: str):
-    """
-    reads the configuration from the YAML file specified
-    returns the config as dictionary object
+def check_chosen_in_available(conf_file):
+    """check that chosen options are actually available (for that task)"""
 
-    Args:
-        config_filepath: path to the YAML file containing the configuration
+    # regresssion
+    assert config.Config.get_chosen_datasets(
+        conf_file, "regression"
+    ) in config.Config.get_available_datasets(
+        conf_file, "regression"
+    ), "chosen dataset for regression is not available"
 
-    """
-    if not (config_filepath.lower().endswith((".yaml", ".yml"))):
-        print("Please provide a path to a YAML file.")
-        quit()
-    with open(config_filepath, "r") as config_file:
-        config = yaml.safe_load(config_file)
-    return config
+    assert config.Config.get_chosen_optimizers(
+        conf_file, "regression"
+    ) in config.Config.get_available_optimizers(
+        conf_file, "regression"
+    ), "chosen optimizer for regression is not available"
 
+    assert config.Config.get_chosen_loss(
+        conf_file, "regression"
+    ) in config.Config.get_available_loss(
+        conf_file, "regression"
+    ), "chosen loss for regression is not available"
 
-def get_available_loss(config):
-    available_loss = []
-    chosen_task = config["task"]["chosen"]
-    if chosen_task == "regression":
-        available_loss = config["loss_functions"]["regression"]["available"]
-    else:
-        available_loss = config["loss_functions"]["classification"]["available"]
-    return available_loss
+    # classification
+    assert config.Config.get_chosen_datasets(
+        conf_file, "classification"
+    ) in config.Config.get_available_datasets(
+        conf_file, "classification"
+    ), "chosen dataset for classification is not available"
 
+    assert config.Config.get_chosen_optimizers(
+        conf_file, "classification"
+    ) in config.Config.get_available_optimizers(
+        conf_file, "classification"
+    ), "chosen optimizer for classification is not available"
 
-def get_chosen_loss(config):
-    chosen_task = config["task"]["chosen"]
-    if chosen_task == "regression":
-        return config["loss_functions"]["regression"]["chosen"]
-    else:
-        return config["loss_functions"]["classification"]["chosen"]
-
-
-def get_available_datasets(config):
-    return config["datasets"]["available"]
-
-
-def get_chosen_datasets(config):
-    return config["datasets"]["chosen"]
-
-
-def get_available_tasks(config):
-    return config["task"]["available"]
+    assert config.Config.get_chosen_loss(
+        conf_file, "classification"
+    ) in config.Config.get_available_loss(
+        conf_file, "classification"
+    ), "chosen loss for classification is not available"
 
 
-def get_chosen_task(config):
-    return config["task"]["chosen"]
+def check_hyperparams(conf_file):
+    """Check that necessary hyperparams are defined"""
+    hyperparams = config.Config.get_hyperparams(conf_file)
+
+    assert (
+        hyperparams["learning_rate"] is not None
+        and hyperparams["learning_rate"] <= 1
+        and hyperparams["learning_rate"] >= 1e-10
+    ), "you sure about that?"
+
+    assert hyperparams["batch_size"] is not None
+
+    assert (
+        hyperparams["num_workers"] is not None
+        and hyperparams["num_workers"] >= 1
+        and hyperparams["num_workers"] <= 32
+    ), "you sure about that?"
+
+    assert (
+        hyperparams["num_epochs"] is not None
+        and hyperparams["num_epochs"] >= 1
+        and hyperparams["num_epochs"] <= 250
+    ), "you sure about that?"
 
 
-def get_wandb_active(config):
-    return config["wandb"]["active_tracking"]
-
-
-def get_wandb_key(config):
-    return config["wandb"]["login_key"]
-
-
-# ============================== asserts ==============================================
-
-
-def check_chosen_in_available(config):
-    assert get_chosen_task(config) in get_available_tasks(
-        config
-    ), "chosen task is not available"
-    assert get_chosen_datasets(config) in get_available_datasets(
-        config
-    ), "chosen dataset is not available"
-    assert get_chosen_loss(config) in get_available_loss(
-        config
-    ), "chosen loss is not available"
-
-
-def wandb_config(config):
-    wandb_key = get_wandb_key(config)
+def wandb_config(conf_file):
+    """check that the wandb key is set up right"""
+    wandb_key = config.Config.get_wandb_key(conf_file)
     assert wandb_key is not None, "put wandb API key in yaml file if you want tracking"
     assert isinstance(
         wandb_key, str
@@ -93,15 +81,15 @@ def wandb_config(config):
 
 
 def main():
-    config_file_location = os.path.join(
-        os.path.dirname(__file__), "..", "central", default_config
-    )
-    config = read_config_file(config_file_location)
+    """run asserts for config file"""
 
-    if get_wandb_active(config):
-        wandb_config(config)
+    actual_config = config.Config(config.Config.DEFAULT_CONFIG)
 
-    check_chosen_in_available(config)
+    if config.Config.get_wand_active(actual_config):
+        wandb_config(actual_config)
+
+    check_chosen_in_available(actual_config)
+    check_hyperparams(actual_config)
 
 
 if __name__ == "__main__":
