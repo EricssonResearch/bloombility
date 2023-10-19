a2fa184659c25b749ba8b0d1f27b9f9ebf77009b
Marion Dübendorfer <marion@duebendorfer.ch>
2023-10-19 12:41:36 +0200

    update main.py

--- bloom/fedavg/main.py
+++ bloom/fedavg/main.py
@@ -1,9 +1,15 @@
-from bloom import load_data
+from bloom.load_data.data_distributor import DATA_DISTRIBUTOR
+from client import generate_client_fn
 
 
 def main():
-    num_clients = 10
-    load_data.data_distributor.DATA_DISTRIBUTOR(num_clients)
+    num_clients = 5
+    data_distributor = DATA_DISTRIBUTOR(num_clients)
+    trainloaders = data_distributor.get_trainloaders()
+    # testloader = data_distributor.get_testloader()
+
+    client_fn = generate_client_fn(trainloaders)
+    return client_fn
 
 
 if __name__ == "__main__":
