b69de3dbfd983c0e8b656839df99e88a761fe184
George Liakopoulos <liakopoulosgeo10@gmail.com>
2023-11-20 15:55:23 +0100

    Trying to work with Popen

--- bloom/FL/main.py
+++ bloom/FL/main.py
@@ -1,5 +1,3 @@
-import wandb
-
 from bloom.load_data.data_distributor import DATA_DISTRIBUTOR
 from client import FlowerClient
 from server import FlowerServer
@@ -7,6 +5,7 @@ from bloom import ROOT_DIR
 
 import os
 import hydra
+import subprocess
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
 import flwr as fl
@@ -19,48 +18,23 @@ def main(cfg: DictConfig):
     print(OmegaConf.to_yaml(cfg))
     # PARAMS
     # Number of rounds of federated learning
-    n_rounds = cfg.server.num_rounds
 
     # Strategies available:  ["FedAvg", "FedAdam", "FedYogi", "FedAdagrad", "FedAvgM"]
-    strategy = cfg.server.strategy
     batch_size = cfg.client.hyper_params.batch_size
     num_epochs = cfg.client.hyper_params.num_epochs
 
     num_clients = cfg.main.num_clients
 
     data_distributor = DATA_DISTRIBUTOR(num_clients)
-    # wandb experiments
-    wandb_track = cfg.main.wandb_active
-    wandb_key = cfg.main.wandb_key
-
-    if wandb_track:
-        wandb.login(anonymous="never", key=wandb_key)
-        # start a new wandb run to track this script
-        wandb.init(
-            # set the wandb project where this run will be logged
-            entity="cs_team_b",
-            project="bloomnet_visualization",
-            # track hyperparameters and run metadata
-            config={
-                "method": "federated",
-                "n_rounds": n_rounds,
-                "strategy": strategy,
-                "clients": num_clients,
-            },
-        )
 
     trainloaders = data_distributor.get_trainloaders()
     testloader = data_distributor.get_testloader()
 
-    server = FlowerServer(strategy=strategy, num_rounds=n_rounds)
-    server.start_server()
+    subprocess.Popen(["./server/server.py"])
 
     for i in range(num_clients):
         client = FlowerClient(trainloaders, testloader, batch_size, num_epochs)
         fl.client.start_numpy_client(server_address="127.0.0.1:8080", client=client)
-
-    if wandb_track:
-        wandb.finish()
 
 
 if __name__ == "__main__":
--- bloom/FL/server/server.py
+++ bloom/FL/server/server.py
@@ -4,8 +4,54 @@ import flwr as fl
 from typing import List
 import numpy as np
 from .utils import get_parameters, define_strategy, weighted_average
-
+import wandb
 from bloom import models
+from bloom import ROOT_DIR
+import os
+import hydra
+from hydra.core.hydra_config import HydraConfig
+from omegaconf import DictConfig, OmegaConf
+
+
+config_path = os.path.join(ROOT_DIR, "config", "federated")
+
+
+@hydra.main(config_path=config_path, config_name="base", version_base=None)
+def main(cfg: DictConfig):
+    print(OmegaConf.to_yaml(cfg))
+
+    # PARAMS
+    # Number of rounds of federated learning
+    n_rounds = cfg.server.num_rounds
+
+    # Strategies available:  ["FedAvg", "FedAdam", "FedYogi", "FedAdagrad", "FedAvgM"]
+    strategy = cfg.server.strategy
+    # wandb experiments
+    wandb_track = cfg.main.wandb_active
+    wandb_key = cfg.main.wandb_key
+    num_clients = cfg.main.num_clients
+
+    if wandb_track:
+        wandb.login(anonymous="never", key=wandb_key)
+        # start a new wandb run to track this script
+        wandb.init(
+            # set the wandb project where this run will be logged
+            entity="cs_team_b",
+            project="bloomnet_visualization",
+            # track hyperparameters and run metadata
+            config={
+                "method": "federated",
+                "n_rounds": n_rounds,
+                "strategy": strategy,
+                "clients": num_clients,
+            },
+        )
+
+    server = FlowerServer(strategy=strategy, num_rounds=n_rounds)
+    server.start_server()
+
+    if wandb_track:
+        wandb.finish()
 
 
 class FlowerServer:
@@ -30,3 +76,7 @@ class FlowerServer:
             config=fl.server.ServerConfig(num_rounds=self.num_rounds),
             strategy=self.strategy,
         )
+
+
+if __name__ == "__main__":
+    main()
