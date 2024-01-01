d3cf3d06587fc1e4c7f0d3aea426d971b0975ef7
mojifj <mojimystery@gmail.com>
2024-01-01 20:31:19 +0100

    add CNNFemnist model for split learning

--- bloom/models/Networks.py
+++ bloom/models/Networks.py
@@ -175,3 +175,32 @@ class ResNet18(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+
+# Split CNNFemnist into worker and head model
+class CNNFemnistWorkerModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 7, padding=3)
+        self.act = nn.ReLU()
+        self.pool = nn.MaxPool2d(2, 2)
+
+    def forward(self, x):
+        x = x.reshape(-1, 1, 28, 28)
+        output = self.pool(self.act(self.conv1(x)))
+        return output
+
+
+# divide femnist into worker and head model
+class CNNFemnistHeadModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.act = nn.ReLU()
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.out = nn.Linear(64 * 7 * 7, 62)
+
+    def forward(self, x):
+        x = self.pool(self.act(self.conv2(x)))
+        x = x.flatten(1)
+        return self.out(x)
